a23cd812c4dd324d9a7d3777169750f4cb75266a
Marino <dmarino8@my.stlcc.edu>
2022-08-18 15:58:11 -0500

    added more canvas

--- reactive-wallpaper/src/Canvas.jsx
+++ reactive-wallpaper/src/Canvas.jsx
@@ -1,40 +1,48 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 
 const Canvas = props => {
-  
-  const canvasRef = useRef(null)
 
-  const draw = (ctx, frameCount) => {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-    ctx.fillStyle = '#a0a6a0'
-    ctx.beginPath()
-    ctx.arc(50, 100, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
-    ctx.fill()
-  }
-  
-  useEffect(() => {
-    
-    const canvas = canvasRef.current
-    const context = canvas.getContext('2d')
-    canvas.width = window.innerWidth
-    canvas.height = window.innerHeight
-    let frameCount = 0
-    let animationFrameId
-    
-    //Our draw came here
-    const render = () => {
-      frameCount++
-      draw(context, frameCount)
-      animationFrameId = window.requestAnimationFrame(render)
+    const canvasRef = useRef(null)
+    const [x, setX] = useState()
+    const [y, setY] = useState()
+
+    const draw = (ctx, frameCount) => {
+        ctx.fillStyle = '#a0a6a0'
+        ctx.beginPath()
+        ctx.arc(x, y, 20, 0, 2 * Math.PI)
+        ctx.fill()
     }
-    render()
-    
-    return () => {
-      window.cancelAnimationFrame(animationFrameId)
-    }
-  }, [draw])
-  
-  return <canvas ref={canvasRef} {...props}/>
+
+    useEffect(() => {
+        const update = (e) => {
+            setX(e.x)
+            setY(e.y)
+        }
+        window.addEventListener('mousemove', update)
+        window.addEventListener('touchmove', update)
+        const canvas = canvasRef.current
+        const context = canvas.getContext('2d')
+        canvas.height = window.innerHeight;
+        canvas.width = window.innerWidth;
+        let frameCount = 0
+        let animationFrameId
+
+        //Our draw came here
+        const render = () => {
+            frameCount++
+            draw(context, frameCount)
+            animationFrameId = window.requestAnimationFrame(render)
+        }
+        render()
+
+        return () => {
+            window.cancelAnimationFrame(animationFrameId)
+            window.removeEventListener('mousemove', update)
+            window.removeEventListener('touchmove', update)
+        }
+    }, [draw, setX, setY])
+
+    return <canvas ref={canvasRef} {...props} />
 }
 
 export default Canvas;
