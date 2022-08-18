d2c075526c8abe6f2ee56307fe39cf5f7ab281b2
User <User@cityofmaplewood.com>
2022-08-18 18:36:24 -0500

    trying to add sprites now

--- reactive-wallpaper/src/Canvas.jsx
+++ reactive-wallpaper/src/Canvas.jsx
@@ -5,18 +5,29 @@ const Canvas = props => {
     const canvasRef = useRef(null)
     const [x, setX] = useState()
     const [y, setY] = useState()
+    const [dir, setDir] = useState(false) // false is left true is right'
+    let img = new Image();
+    img.src = 'sprite-greenS-blueP-rightF.png';
 
     const draw = (ctx, frameCount) => {
-        ctx.fillStyle = '#a0a6a0'
-        ctx.beginPath()
-        ctx.arc(x, y, 20, 0, 2 * Math.PI)
-        ctx.fill()
+        //ctx.fillStyle = '#a0a6a0'
+        //ctx.beginPath()
+        //ctx.arc(x, y, 20, 0, 2 * Math.PI)
+        //ctx.fill()
+            ctx.drawImage(img, 20, 20);
     }
+    
 
     useEffect(() => {
         const update = (e) => {
+            if (e.x < x) {
+                setDir(false);
+            } else if (e.x > x) {
+                setDir(true);
+            }
             setX(e.x)
             setY(e.y)
+            console.log(dir);
         }
         window.addEventListener('mousemove', update)
         window.addEventListener('touchmove', update)
@@ -26,14 +37,12 @@ const Canvas = props => {
         canvas.width = window.innerWidth;
         let frameCount = 0
         let animationFrameId
-
-        //Our draw came here
         const render = () => {
             frameCount++
             draw(context, frameCount)
             animationFrameId = window.requestAnimationFrame(render)
         }
-        render()
+        setInterval(render(), 1);
 
         return () => {
             window.cancelAnimationFrame(animationFrameId)
